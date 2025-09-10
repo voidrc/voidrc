dad3658accbe1d32cd1c32c0c340fa1846b632db
Void Strike <voidrc_10101@proton.me>
2025-09-10 07:17:17 +0500

    Fuck you all

--- CTFs/BlackHat-2025/cute_csp/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export ADMIN_TOKEN=$(openssl rand -hex 16)
-export PLAYWRIGHT_BROWSERS_PATH="/opt/playwright"
-echo "ADMIN_TOKEN = $ADMIN_TOKEN"
-exec apache2-foreground
--- CTFs/BlackHat-2025/cute_csp/src/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline; img-src *;");
-@print($_GET["html"] ?? show_source(__FILE__));
